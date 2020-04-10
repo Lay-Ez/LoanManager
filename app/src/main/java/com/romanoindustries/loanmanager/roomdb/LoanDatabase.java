4daed3a174951f680b9b9eacc0274615abe7238d
Roman <deep.depp@yandex.ru>
2020-04-10 20:11:02 +0300

    add roomdatabase

---
+++ app/src/main/java/com/romanoindustries/loanmanager/roomdb/LoanDatabase.java
@@ -0,0 +1,30 @@
+package com.romanoindustries.loanmanager.roomdb;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.romanoindustries.loanmanager.datamodel.Loan;
+
+@Database(entities = Loan.class, version = 1)
+public abstract class LoanDatabase extends RoomDatabase {
+
+    private static LoanDatabase instance;
+
+    public abstract LoanDao loanDao();
+
+    public static synchronized LoanDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    LoanDatabase.class,
+                    "loan_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return instance;
+    }
+
+}
