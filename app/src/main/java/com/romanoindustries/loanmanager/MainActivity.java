7cc016472fa0f0edd65b504a6054a28fbe6c41ea
Roman <deep.depp@yandex.ru>
2020-04-11 17:48:20 +0300

    place last viewed fragment into layout container

--- app/src/main/java/com/romanoindustries/loanmanager/MainActivity.java
+++ app/src/main/java/com/romanoindustries/loanmanager/MainActivity.java
@@ -15,8 +15,16 @@ import com.romanoindustries.loanmanager.fragments.OutgoingLoansFragment;
 public class MainActivity extends AppCompatActivity {
 
     private IncomingLoansFragment incomingLoansFragment;
+    private static final int IN_FRAGMENT_ID = 1;
+
     private OutgoingLoansFragment outgoingLoansFragment;
+    private static final int OUT_FRAGMENT_ID = 2;
+
     private ArchivedLoansFragment archivedLoansFragment;
+    private static final int ARCH_FRAGMENT_ID = 3;
+
+    public static final String CURRENT_FRAGMENT_KEY = "current_fragment";
+    private int currentlFragmentId = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +37,35 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
         navigationView.setOnNavigationItemSelectedListener(navListener);
 
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, incomingLoansFragment).commit();
+        restoreViewedFragment(savedInstanceState);
+    }
+
+    private void restoreViewedFragment(Bundle bundle) {
+        if (bundle == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, incomingLoansFragment).commit();
+            return;
+        }
+
+        int viewFragmentId = bundle.getInt(CURRENT_FRAGMENT_KEY, IN_FRAGMENT_ID);
+
+        if (viewFragmentId == IN_FRAGMENT_ID) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, incomingLoansFragment).commit();
+        } else if (viewFragmentId == OUT_FRAGMENT_ID) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, outgoingLoansFragment).commit();
+        } else if (viewFragmentId == ARCH_FRAGMENT_ID){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, archivedLoansFragment).commit();
+        }
+        currentlFragmentId = viewFragmentId;
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(CURRENT_FRAGMENT_KEY, currentlFragmentId);
+        super.onSaveInstanceState(outState);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -43,41 +78,24 @@ public class MainActivity extends AppCompatActivity {
 
                         case R.id.nav_loans_in:
                             selectedFragment = incomingLoansFragment;
+                            currentlFragmentId = IN_FRAGMENT_ID;
                             break;
 
                         case R.id.nav_loans_out:
                             selectedFragment = outgoingLoansFragment;
+                            currentlFragmentId = OUT_FRAGMENT_ID;
                             break;
 
                         case R.id.nav_loans_history:
                             selectedFragment = archivedLoansFragment;
+                            currentlFragmentId = ARCH_FRAGMENT_ID;
                             break;
-
-
                     }
                     getSupportFragmentManager().beginTransaction()
                             .replace(R.id.fragment_container, selectedFragment).commit();
-
                     return true;
                 }
             };
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
