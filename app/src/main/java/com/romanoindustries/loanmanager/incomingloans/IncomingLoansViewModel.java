514c7a8ca4e5c27d123b54fbbe1e24d311c01da6
Roman <deep.depp@yandex.ru>
2020-04-11 17:09:46 +0300

    add viewmodel for all loans

--- app/src/main/java/com/romanoindustries/loanmanager/incomingloans/IncomingLoansViewModel.java
+++
@@ -1,6 +0,0 @@
-package com.romanoindustries.loanmanager.incomingloans;
-
-import androidx.lifecycle.ViewModel;
-
-public class IncomingLoansViewModel extends ViewModel {
-}
---
+++ app/src/main/java/com/romanoindustries/loanmanager/viewmodels/LoansViewModel.java
@@ -0,0 +1,56 @@
+package com.romanoindustries.loanmanager.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.romanoindustries.loanmanager.datamodel.Loan;
+import com.romanoindustries.loanmanager.loanrepo.LoanRepo;
+
+import java.util.List;
+
+public class LoansViewModel extends AndroidViewModel {
+
+    private LoanRepo loanRepo;
+    private LiveData<List<Loan>> inLoans;
+    private LiveData<List<Loan>> outLoans;
+    private LiveData<List<Loan>> archivedLoans;
+
+    public LoansViewModel(@NonNull Application application) {
+        super(application);
+        loanRepo = new LoanRepo(application);
+        inLoans = loanRepo.getInLoans();
+        outLoans = loanRepo.getOutLoans();
+        archivedLoans = loanRepo.getArchivedLoans();
+    }
+
+    public void insert(Loan loan) {
+        loanRepo.insert(loan);
+    }
+
+    public void update(Loan loan) {
+        loanRepo.update(loan);
+    }
+
+    public void delete(Loan loan) {
+        loanRepo.delete(loan);
+    }
+
+    public void deleteAllLoans() {
+        loanRepo.deleteAllLoans();
+    }
+
+    public LiveData<List<Loan>> getInLoans() {
+        return inLoans;
+    }
+
+    public LiveData<List<Loan>> getOutLoans() {
+        return outLoans;
+    }
+
+    public LiveData<List<Loan>> getArchivedLoans() {
+        return archivedLoans;
+    }
+}
