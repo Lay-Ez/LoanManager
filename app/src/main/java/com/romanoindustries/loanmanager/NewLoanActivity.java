35516afca1b636752171f0947515e6c59cfea0af
Roman <deep.depp@yandex.ru>
2020-04-12 15:09:22 +0300

    add new loan activity

---
+++ app/src/main/java/com/romanoindustries/loanmanager/NewLoanActivity.java
@@ -0,0 +1,14 @@
+package com.romanoindustries.loanmanager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewLoanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_loan);
+    }
+}
--- app/src/main/java/com/romanoindustries/loanmanager/fragments/IncomingLoansFragment.java
+++ app/src/main/java/com/romanoindustries/loanmanager/fragments/IncomingLoansFragment.java
@@ -1,8 +1,8 @@
 package com.romanoindustries.loanmanager.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +13,11 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.romanoindustries.loanmanager.NewLoanActivity;
 import com.romanoindustries.loanmanager.R;
-import com.romanoindustries.loanmanager.datamodel.InterestChargeEvent;
 import com.romanoindustries.loanmanager.datamodel.Loan;
 import com.romanoindustries.loanmanager.viewmodels.LoansViewModel;
 
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -54,7 +52,6 @@ public class IncomingLoansFragment extends Fragment {
         });
 
         initViews(view);
-
         return view;
     }
 
@@ -65,28 +62,8 @@ public class IncomingLoansFragment extends Fragment {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Loan testLoan = new Loan();
-                testLoan.setDebtorName("Test name");
-                testLoan.setSpecialNote("Test special note");
-                testLoan.setStartAmount(1000);
-                testLoan.setCurrentAmount(1010);
-                testLoan.setPhoneNumber("88005553535");
-
-                Calendar calendar = Calendar.getInstance();
-                testLoan.setStartDateInMs(calendar.getTimeInMillis());
-
-                calendar.add(Calendar.DAY_OF_YEAR, 10);
-                testLoan.setPaymentDateInMs(calendar.getTimeInMillis());
-
-                calendar.add(Calendar.WEEK_OF_YEAR, 10);
-                testLoan.setNextChargingDateInMs(calendar.getTimeInMillis());
-                testLoan.setPeriodInDays(10);
-                testLoan.setInterestRate(1.5);
-                testLoan.setType(Loan.TYPE_IN);
-                testLoan.setChargeEvents(new ArrayList<InterestChargeEvent>());
-
-                loansViewModel.insert(testLoan);
-                Log.d(TAG, "onClick: new loan inserted");
+                Intent intent = new Intent(getContext(), NewLoanActivity.class);
+                startActivity(intent);
             }
         });
     }
