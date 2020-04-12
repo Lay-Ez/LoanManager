d03b21e6125ef5d7f94a67c9c1c3558f224cf8c6
Roman <deep.depp@yandex.ru>
2020-04-12 19:59:55 +0300

    implement basic add loan interface layout

--- app/src/main/java/com/romanoindustries/loanmanager/NewLoanActivity.java
+++ app/src/main/java/com/romanoindustries/loanmanager/NewLoanActivity.java
@@ -1,23 +1,39 @@
 package com.romanoindustries.loanmanager;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.romanoindustries.loanmanager.fragments.DatePickerFragment;
 
-public class NewLoanActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class NewLoanActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     private static final String TAG = "NewLoanActivity";
 
     private TextInputLayout inputLayoutName;
     private TextInputLayout inputLayoutPhone;
+    private TextInputEditText editTextName;
+    private TextInputEditText editTextPhone;
+    private Button endDateBtn;
+    private CheckBox enableInterestCb;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +46,8 @@ public class NewLoanActivity extends AppCompatActivity {
     private void initViews() {
         inputLayoutName = findViewById(R.id.text_input_name);
         inputLayoutPhone = findViewById(R.id.text_input_phone);
+        editTextName = findViewById(R.id.edit_text_name);
+        editTextPhone = findViewById(R.id.edit_text_phone);
 
         inputLayoutName.setEndIconOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +55,23 @@ public class NewLoanActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
                 startActivityForResult(intent, RESULT_FIRST_USER);
+            }
+        });
+
+        endDateBtn = findViewById(R.id.end_date_btn);
+        endDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date_picker");
+            }
+        });
+
+        enableInterestCb = findViewById(R.id.enable_interest_cb);
+        enableInterestCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //
             }
         });
     }
@@ -65,8 +100,19 @@ public class NewLoanActivity extends AppCompatActivity {
             String number = cursor.getString(numberColumnIndex);
             String name = cursor.getString(nameColumnIndex);
 
-            Log.d(TAG, "parseContactInfo: name=" + name + " phone=" + number);
+            editTextPhone.setText(number);
+            editTextName.setText(name);
         }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.YEAR, year);
+        String dateString = DateFormat.getDateInstance().format(calendar.getTime());
+        endDateBtn.setText(dateString);
     }
 }
 
