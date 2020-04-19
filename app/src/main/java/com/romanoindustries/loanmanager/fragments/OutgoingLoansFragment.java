4fb1979c8f54952cfec56d9d3e540cefe7c52165
Roman <deep.depp@yandex.ru>
2020-04-19 19:31:26 +0300

    layout fixes

--- app/src/main/java/com/romanoindustries/loanmanager/fragments/OutgoingLoansFragment.java
+++ app/src/main/java/com/romanoindustries/loanmanager/fragments/OutgoingLoansFragment.java
@@ -1,31 +1,59 @@
 package com.romanoindustries.loanmanager.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.romanoindustries.loanmanager.R;
+import com.romanoindustries.loanmanager.datamodel.Loan;
+import com.romanoindustries.loanmanager.newloan.NewLoanActivity;
+import com.romanoindustries.loanmanager.viewmodels.LoansViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class OutgoingLoansFragment extends Fragment {
+    private static final String TAG = "OutgoingLoansFragment";
 
+    private FloatingActionButton fab;
+    private LoansViewModel loansViewModel;
 
-    public OutgoingLoansFragment() {
-        // Required empty public constructor
-    }
+    public OutgoingLoansFragment() {}
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_outgoing_loans, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_outgoing_loans, container, false);
+
+        loansViewModel = new ViewModelProvider
+                .AndroidViewModelFactory(getActivity().getApplication())
+                .create(LoansViewModel.class);
+
+        loansViewModel.getOutLoans().observe(this, loans -> loans.forEach(loan -> {
+            Log.d(TAG, "onCreateView: " + loan);
+        }));
+
+        initViews(view);
+        return view;
     }
 
+    private void initViews(View view) {
+        RecyclerView recyclerView = view.findViewById(R.id.out_loans_recycler_view);
+
+        fab = view.findViewById(R.id.out_loans_fab);
+        fab.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), NewLoanActivity.class);
+            intent.putExtra(NewLoanActivity.LOAN_TYPE_KEY, Loan.TYPE_OUT);
+            startActivity(intent);
+        });
+    }
 }
