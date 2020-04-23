85483c4512eca49a86d6d5ad40740ea2fb561d28
Roman <deep.depp@yandex.ru>
2020-04-23 19:37:35 +0300

    display all loan fields in loan info activity

--- app/src/main/java/com/romanoindustries/loanmanager/viewloaninfo/LoanInfoActivity.java
+++ app/src/main/java/com/romanoindustries/loanmanager/viewloaninfo/LoanInfoActivity.java
@@ -2,37 +2,31 @@ package com.romanoindustries.loanmanager.viewloaninfo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.romanoindustries.loanmanager.R;
+import com.romanoindustries.loanmanager.databinding.ActivityLoanInfoBinding;
 import com.romanoindustries.loanmanager.datamodel.Loan;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class LoanInfoActivity extends AppCompatActivity {
     private static final String TAG = "LoanInfoActivity";
     public static final String LOAN_ID_KEY = "loan_id_key";
-
-    private TextView loanTypeTv;
-    private TextView nameTv;
-    private TextView phoneTv;
-    private TextView startDateTv;
-    private TextView endDateTv;
-    private TextView nextAccrualDateTv;
-    private TextView startAmountTv;
-    private TextView currentAmountTv;
-    private TextView interestRateTv;
-    private TextView interestPeriodTv;
-    private TextView idTv;
-    private TextView noteTv;
+    private ActivityLoanInfoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loan_info);
-        initViews();
+        binding = ActivityLoanInfoBinding.inflate(getLayoutInflater());
+        View rootView = binding.getRoot();
+        setContentView(rootView);
         Intent intent = getIntent();
         int loanId = intent.getIntExtra(LOAN_ID_KEY, -1);
         if (loanId == -1) {
@@ -53,25 +47,107 @@ public class LoanInfoActivity extends AppCompatActivity {
         });
     }
 
-    private void initViews() {
-        loanTypeTv = findViewById(R.id.info_loan_type_tv);
-        nameTv = findViewById(R.id.info_name_tv);
-        phoneTv = findViewById(R.id.info_phone_tv);
-        startDateTv = findViewById(R.id.info_start_date_tv);
-        endDateTv = findViewById(R.id.info_end_date_tv);
-        nextAccrualDateTv = findViewById(R.id.info_next_accrual_tv);
-        startAmountTv = findViewById(R.id.info_start_amount_tv);
-        currentAmountTv = findViewById(R.id.info_current_amount_tv);
-        interestRateTv = findViewById(R.id.info_interest_rate_tv);
-        interestPeriodTv = findViewById(R.id.info_period_tv);
-        idTv = findViewById(R.id.info_id_tv);
-        noteTv = findViewById(R.id.info_note_tv);
-    }
-
     private void displayLoan(Loan loan) {
         String name = loan.getDebtorName();
         if (!name.isEmpty()) {
-            nameTv.setText(name);
+            binding.infoNameTv.setText(name);
         }
+
+        String phone = loan.getPhoneNumber();
+        Log.d(TAG, "displayLoan: phone=" + phone);
+
+        if (!phone.equals("")) {
+            binding.infoPhoneTv.setText(phone);
+        } else {
+            binding.infoPhoneTv.setText(R.string.not_specified_string);
+        }
+
+        binding.infoStartDateTv.setText(formatDate(loan.getStartDateInMs()));
+
+        if (loan.getPaymentDateInMs() != 0) {
+            binding.infoEndDateTv.setText(formatDate(loan.getPaymentDateInMs()));
+        } else {
+            binding.infoEndDateTv.setText(R.string.not_specified_string);
+        }
+
+        if (loan.getNextChargingDateInMs() != 0) {
+            binding.infoNextAccrualTv.setText(formatDate(loan.getNextChargingDateInMs()));
+        } else {
+            binding.infoNextAccrualTv.setText(R.string.not_specified_string);
+        }
+
+        int startAmount = loan.getStartAmount();
+        binding.infoStartAmountTv.setText(String.valueOf(startAmount));
+
+        int currentAmount = loan.getCurrentAmount();
+        binding.infoCurrentAmountTv.setText(String.valueOf(currentAmount));
+
+        double interestRate = loan.getInterestRate();
+        if (interestRate > 0.0) {
+            String rateStr = interestRate + "%";
+            binding.infoInterestRateTv.setText(rateStr);
+        } else {
+            binding.infoInterestRateTv.setText(R.string.not_specified_string);
+        }
+
+        int periodInDays = loan.getPeriodInDays();
+        if (periodInDays != 0) {
+            binding.infoPeriodTv.setText(String.valueOf(periodInDays));
+        } else {
+            binding.infoPeriodTv.setText(R.string.not_specified_string);
+        }
+
+        binding.infoIdTv.setText(String.valueOf(loan.getId()));
+
+        String note = loan.getSpecialNote();
+        if (!note.equals("")) {
+            binding.infoNoteTv.setText(note);
+        } else {
+            binding.infoNoteTv.setText(R.string.not_specified_string);
+        }
+
+        String loanType = getString(R.string.not_specified_string);
+        switch (loan.getType()) {
+
+            case Loan.TYPE_IN:
+                loanType = getString(R.string.loan_type_incoming);
+                break;
+
+            case Loan.TYPE_OUT:
+                loanType = getString(R.string.loan_type_outgoing);
+                break;
+
+            case Loan.TYPE_ARCHIVED_IN:
+                loanType = getString(R.string.loan_type_archived_in);
+                break;
+
+            case Loan.TYPE_ARCHIVED_OUT:
+                loanType = getString(R.string.loan_type_archived_out);
+                break;
+        }
+        binding.infoLoanTypeTv.setText(loanType);
+    }
+
+    private String formatDate(long dateMs) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(dateMs);
+        return DateFormat.getDateInstance().format(calendar.getTime());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
