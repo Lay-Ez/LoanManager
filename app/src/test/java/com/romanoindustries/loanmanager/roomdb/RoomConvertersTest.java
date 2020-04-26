b12b26774757e403a7fa56d1e375199d844be47a
Roman <deep.depp@yandex.ru>
2020-04-26 21:46:56 +0300

    change test for room converters to use assertions

--- app/src/test/java/com/romanoindustries/loanmanager/roomdb/RoomConvertersTest.java
+++ app/src/test/java/com/romanoindustries/loanmanager/roomdb/RoomConvertersTest.java
@@ -4,12 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.romanoindustries.loanmanager.datamodel.InterestAccrualEvent;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class RoomConvertersTest {
 
@@ -17,7 +17,6 @@ public class RoomConvertersTest {
     @Test
     public void testCalendarWithGson() {
 
-        List<InterestAccrualEvent> chargeEvents = new ArrayList<>();
         Calendar calendar1 = Calendar.getInstance();
         Calendar calendar2 = Calendar.getInstance();
         Calendar calendar3 = Calendar.getInstance();
@@ -28,6 +27,7 @@ public class RoomConvertersTest {
         InterestAccrualEvent event3 = new InterestAccrualEvent(calendar3, 234234, 234234);
         InterestAccrualEvent event4 = new InterestAccrualEvent(calendar4, 1444, 24234);
 
+        List<InterestAccrualEvent> chargeEvents = new ArrayList<>();
         chargeEvents.add(event1);
         chargeEvents.add(event2);
         chargeEvents.add(event3);
@@ -37,13 +37,23 @@ public class RoomConvertersTest {
 
         String json1 = gson.toJson(chargeEvents);
         ArrayList<InterestAccrualEvent> chargeEventsResult = gson.fromJson(json1, new TypeToken<List<InterestAccrualEvent>>(){}.getType());
-        chargeEventsResult.forEach(new Consumer<InterestAccrualEvent>() {
-            @Override
-            public void accept(InterestAccrualEvent interestAccrualEvent) {
-                System.out.println(interestAccrualEvent.getChargeEventDate().getTimeInMillis());
-                System.out.println(interestAccrualEvent.getStartAmount());
-                System.out.println(interestAccrualEvent.getEndAmount());
-            }
+        chargeEventsResult.forEach(interestAccrualEvent -> {
+            System.out.println(interestAccrualEvent.getChargeEventDate().getTimeInMillis());
+            System.out.println(interestAccrualEvent.getStartAmount());
+            System.out.println(interestAccrualEvent.getEndAmount());
         });
+
+        for (int i = 0; i < chargeEvents.size() - 1; i++) {
+            InterestAccrualEvent originalEvent = chargeEvents.get(i);
+            InterestAccrualEvent resultedEvent = chargeEventsResult.get(i);
+            Assert.assertEquals(originalEvent.getStartAmount(), resultedEvent.getStartAmount());
+            Assert.assertEquals(originalEvent.getEndAmount(), resultedEvent.getEndAmount());
+            long originalTime = originalEvent.getChargeEventDate().getTimeInMillis();
+            long resultedTime = resultedEvent.getChargeEventDate().getTimeInMillis();
+            /* to drop last three digits */
+            originalTime -= originalTime % 1000;
+            resultedTime -= resultedTime % 1000;
+            Assert.assertEquals(originalTime, resultedTime);
+        }
     }
 }
